5b04667178c5ad1fb515e2b1e0a920ae63f35623
Nils Sommer <mail@nilssommer.de>
2013-10-30 13:18:16 +0100

    .gitignore and sql script Added eclipse project files to .gitignore file. Added basic sql script to create some tables for testing.

---
+++ webkochbuch_createtables.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE recipes (
+	id integer PRIMARY KEY,
+	title varchar(100) NOT NULL,
+	description varchar(255) NOT NULL,
+	content text NOT NULL,
+	preparation_endurance integer NOT NULL,
+	total_endurance integer NOT NULL,
+	creation timestamp without time zone NOT NULL
+);
+
+CREATE TABLE images (
+	id integer PRIMARY KEY,
+	recipe_id integer REFERENCES recipes(id),
+	file_path varchar(255) NOT NULL,
+	description varchar(255)
+);
+
+CREATE TABLE ratings (
+	id integer PRIMARY KEY,
+	recipe_id integer REFERENCES recipes(id),
+	user_id integer,
+	rating integer NOT NULL,
+	creation timestamp without time zone NOT NULL
+);
