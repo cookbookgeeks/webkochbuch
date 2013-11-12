e5b22bce3d1ef5bcbf8aad51ef7c173e45668fa8
Nils Sommer <mail@nilssommer.de>
2013-11-12 15:34:59 +0100

    Added hibernate files (still highly experimental)

---
+++ src/main/java/org/cookbookgeeks/webkochbuch/configuration/HibernateConfiguration.java
@@ -0,0 +1,41 @@
+package org.cookbookgeeks.webkochbuch.configuration;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.cookbookgeeks.webkochbuch.domain.Recipe;
+import org.hibernate.dialect.H2Dialect;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate3.HibernateTransactionManager;
+import org.springframework.orm.hibernate3.annotation.AnnotationSessionFactoryBean;
+
+@Configuration
+public class HibernateConfiguration {
+	
+
+	@Value("#{dataSource}")
+	private DataSource dataSource;
+
+	@Bean
+	public AnnotationSessionFactoryBean sessionFactoryBean() {
+		Properties props = new Properties();
+		props.put("hibernate.dialect", H2Dialect.class.getName());
+		props.put("hibernate.format_sql", "true");
+
+		AnnotationSessionFactoryBean bean = new AnnotationSessionFactoryBean();
+		bean.setAnnotatedClasses(new Class[]{Recipe.class});		
+		bean.setHibernateProperties(props);
+		bean.setDataSource(this.dataSource);
+		bean.setSchemaUpdate(true);
+		return bean;
+	}
+
+	@Bean
+	public HibernateTransactionManager transactionManager() {
+		return new HibernateTransactionManager( sessionFactoryBean().getObject() );
+	}
+
+}
